ea3a7389714f5b299aa9b498f3e7338782738bca
eko5624 <562433403@qq.com>
2023-04-29 20:42:46 +0800

    Update toolchain.cmake

--- toolchain.cmake
+++ toolchain.cmake
@@ -3,14 +3,14 @@ set(CMAKE_SYSTEM_PROCESSOR x86_64)
 
 set(triple x86_64-w64-mingw32)
 
-set(CMAKE_C_COMPILER ${triple}-gcc)
-set(CMAKE_CXX_COMPILER ${triple}-g++)
-set(CMAKE_RC_COMPILER ${triple}-windres)
-set(CMAKE_RANLIB ${triple}-gcc-ranlib)
-set(CMAKE_AR ${triple}-gcc-ar)
-set(CMAKE_ASM_COMPILER ${triple}-gcc-as)
+set(CMAKE_C_COMPILER $M_CROSS/bin/${triple}-gcc)
+set(CMAKE_CXX_COMPILER $M_CROSS/bin/${triple}-g++)
+set(CMAKE_RC_COMPILER $M_CROSS/bin/${triple}-windres)
+set(CMAKE_RANLIB $M_CROSS/bin/${triple}-ranlib)
+set(CMAKE_AR $M_CROSS/bin/${triple}-ar)
+set(CMAKE_ASM_COMPILER $M_CROSS/bin/${triple}-as)
 
-set(CMAKE_FIND_ROOT_PATH $M_CROSS/x86_64-w64-mingw32)
+set(CMAKE_FIND_ROOT_PATH $M_CROSS/opt)
 
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
