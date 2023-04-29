22205faebf9f8a942f7b04956889513b3a0aa60c
eko5624 <562433403@qq.com>
2023-04-29 20:16:37 +0800

    Update toolchain.cmake

--- toolchain.cmake
+++ toolchain.cmake
@@ -10,7 +10,7 @@ set(CMAKE_RANLIB ${triple}-gcc-ranlib)
 set(CMAKE_AR ${triple}-gcc-ar)
 set(CMAKE_ASM_COMPILER ${triple}-gcc-as)
 
-set(CMAKE_FIND_ROOT_PATH $TOP_DIR/opt)
+set(CMAKE_FIND_ROOT_PATH $M_CROSS/bin)
 
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
