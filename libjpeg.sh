8e7632cda8d827725736c1f84efa95502976fd0a
eko5624 <562433403@qq.com>
2023-04-29 22:19:06 +0800

    Update libjpeg.sh

--- libjpeg.sh
+++ libjpeg.sh
@@ -14,11 +14,9 @@ MJOBS=$(grep -c processor /proc/cpuinfo)
 
 MINGW_TRIPLE="x86_64-w64-mingw32"
 
-export PATH="$M_CROSS/bin:$RUSTUP_LOCATION/.cargo/bin:$PATH"
+export PATH="$M_CROSS/bin:$PATH"
 export PKG_CONFIG="pkgconf --static"
 export PKG_CONFIG_LIBDIR="$M_CROSS/opt/lib/pkgconfig"
-export RUSTUP_HOME="$RUSTUP_LOCATION/.rustup"
-export CARGO_HOME="$RUSTUP_LOCATION/.cargo"
 
 cd $TOP_DIR
 git clone https://github.com/libjpeg-turbo/libjpeg-turbo.git
