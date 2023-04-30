8759eb37e67ef5071d539dc560f9e06f8b67393d
eko5624 <562433403@qq.com>
2023-04-30 09:14:11 +0800

    Update libjpeg.sh

--- libjpeg.sh
+++ libjpeg.sh
@@ -14,9 +14,20 @@ MJOBS=$(grep -c processor /proc/cpuinfo)
 
 MINGW_TRIPLE="x86_64-w64-mingw32"
 
-export PATH="$M_CROSS/bin:$PATH"
+PATH="$M_CROSS/bin:$RUSTUP_LOCATION/.cargo/bin:$PATH"
 export PKG_CONFIG="pkgconf --static"
 export PKG_CONFIG_LIBDIR="$M_CROSS/opt/lib/pkgconfig"
+
+export CC=$M_CROSS/bin/$MINGW_TRIPLE-gcc
+export CXX=$M_CROSS/bin/$MINGW_TRIPLE-g++
+export AR=$M_CROSS/bin/$MINGW_TRIPLE-ar
+export RANLIB=$M_CROSS/bin/$MINGW_TRIPLE-ranlib
+export AS=$M_CROSS/bin/$MINGW_TRIPLE-as
+export LD=$M_CROSS/bin/$MINGW_TRIPLE-ld
+export STRIP=$M_CROSS/bin/$MINGW_TRIPLE-strip
+export NM=$M_CROSS/bin/$MINGW_TRIPLE-nm
+export DLLTOOL=$M_CROSS/bin/$MINGW_TRIPLE-dlltool
+export WINDRES=$M_CROSS/bin/$MINGW_TRIPLE-windres
 
 cd $TOP_DIR
 git clone https://github.com/libjpeg-turbo/libjpeg-turbo.git
