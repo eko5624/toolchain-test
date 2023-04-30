4e31959829af15d384ee6c174723608d7682043c
eko5624 <562433403@qq.com>
2023-04-30 17:28:09 +0800

    Update toolchain.cmake

--- toolchain.cmake
+++ toolchain.cmake
@@ -3,10 +3,10 @@ set(CMAKE_SYSTEM_PROCESSOR x86_64)
 
 set(triple x86_64-w64-mingw32)
 
-set(CMAKE_C_COMPILER ${triple}-gcc)
-set(CMAKE_CXX_COMPILER ${triple}-g++)
-set(CMAKE_RC_COMPILER ${triple}-windres)
-set(CMAKE_ASM_COMPILER ${triple}-as)
+set(CMAKE_C_COMPILER /cross/bin/${triple}-gcc)
+set(CMAKE_CXX_COMPILER /cross/bin/${triple}-g++)
+set(CMAKE_RC_COMPILER /cross/bin/${triple}-windres)
+set(CMAKE_ASM_COMPILER /cross/bin/${triple}-as)
 
 set(CMAKE_FIND_ROOT_PATH @TOP_DIR@/opt)
 set(CMAKE_INSTALL_PREFIX @TOP_DIR@/opt)
