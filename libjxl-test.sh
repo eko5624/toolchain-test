39d9f46f054f4ed643c92da4623d97ad66fd82e5
eko5624 <562433403@qq.com>
2023-05-01 19:51:11 +0800

    Update libjxl-test.sh

--- libjxl-test.sh
+++ libjxl-test.sh
@@ -3,16 +3,16 @@
 # basic param and command line option to change it
 set -e
 
-TOP_DIR=$(pwd)
-M_CROSS=$TOP_DIR/cross
-RUSTUP_LOCATION=$TOP_DIR/rustup_location
+export TOP_DIR=$(pwd)
+export M_CROSS=$TOP_DIR/cross
+export RUSTUP_LOCATION=$TOP_DIR/rustup_location
 
 # Speed up the process
 # Env Var NUMJOBS overrides automatic detection
 MJOBS=$(grep -c processor /proc/cpuinfo)
 
 
-MINGW_TRIPLE="x86_64-w64-mingw32"
+export MINGW_TRIPLE="x86_64-w64-mingw32"
 
 export PATH="$M_CROSS/bin:$RUSTUP_LOCATION/.cargo/bin:$PATH"
 export PKG_CONFIG="pkgconf --static"
@@ -22,18 +22,18 @@ export CARGO_HOME="$RUSTUP_LOCATION/.cargo"
 
 export CC=$M_CROSS/bin/$MINGW_TRIPLE-gcc
 export CXX=$M_CROSS/bin/$MINGW_TRIPLE-g++
-export AR=$M_CROSS/bin/$MINGW_TRIPLE-ar
-export RANLIB=$M_CROSS/bin/$MINGW_TRIPLE-ranlib
+export AR=$M_CROSS/bin/$MINGW_TRIPLE-gcc-ar
+export RANLIB=$M_CROSS/bin/$MINGW_TRIPLE-gcc-ranlib
 export AS=$M_CROSS/bin/$MINGW_TRIPLE-as
 export LD=$M_CROSS/bin/$MINGW_TRIPLE-ld
 export STRIP=$M_CROSS/bin/$MINGW_TRIPLE-strip
-export NM=$M_CROSS/bin/$MINGW_TRIPLE-nm
+export NM=$M_CROSS/bin/$MINGW_TRIPLE-gcc-nm
 export DLLTOOL=$M_CROSS/bin/$MINGW_TRIPLE-dlltool
 export WINDRES=$M_CROSS/bin/$MINGW_TRIPLE-windres
 
-export CFLAGS="-static-libgcc -static-libstdc++ -I$TOP_DIR/opt/include"
-export CXXFLAGS="-static-libgcc -static-libstdc++ -I$TOP_DIR/opt/include"
-export LDFLAGS="-static-libgcc -static-libstdc++ -L$TOP_DIR/opt/lib"
+CFLAGS="-I$TOP_DIR/opt/include"
+CXXFLAGS="-I$TOP_DIR/opt/include"
+LDFLAGS="-L$TOP_DIR/opt/lib"
 
 echo "building brotli"
 echo "======================="
