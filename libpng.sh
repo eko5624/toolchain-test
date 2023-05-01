1808b4093c8135559dc360a309f5963b63bd392c
eko5624 <562433403@qq.com>
2023-05-01 16:54:44 +0800

    Update libpng.sh

--- libpng.sh
+++ libpng.sh
@@ -13,20 +13,19 @@ MJOBS=$(grep -c processor /proc/cpuinfo)
 
 export MINGW_TRIPLE="x86_64-w64-mingw32"
 
-export PATH="$M_CROSS/bin:$RUSTUP_LOCATION/.cargo/bin:$PATH"
-export PKG_CONFIG="pkgconf --static"
-export PKG_CONFIG_LIBDIR="$M_CROSS/opt/lib/pkgconfig"
-export RUSTUP_HOME="$RUSTUP_LOCATION/.rustup"
-export CARGO_HOME="$RUSTUP_LOCATION/.cargo"
-
 cd $TOP_DIR
 curl -OL https://github.com/eko5624/toolchain-test/releases/download/dev/zlib.7z
 for f in *.7z; do 7z x "$f"; done
 git clone https://github.com/glennrp/libpng.git
 cd libpng
+export PATH="$M_CROSS/bin:$RUSTUP_LOCATION/.cargo/bin:$PATH"
+export PKG_CONFIG="pkgconf --static"
+export PKG_CONFIG_LIBDIR="$M_CROSS/opt/lib/pkgconfig"
+export RUSTUP_HOME="$RUSTUP_LOCATION/.rustup"
+export CARGO_HOME="$RUSTUP_LOCATION/.cargo"
+export CFLAGS='-fno-asynchronous-unwind-tables'
 autoreconf -ivf
 ./configure \
-  CFLAGS='-fno-asynchronous-unwind-tables' \
   --host=$MINGW_TRIPLE \
   --prefix=$TOP_DIR/opt \
   --disable-shared
