a76988574dc56f368498694549a58065e996ed18
eko5624 <562433403@qq.com>
2023-05-02 14:10:28 +0800

    Update libjxl-test.sh

--- libjxl-test.sh
+++ libjxl-test.sh
@@ -64,6 +64,11 @@ git clone https://github.com/madler/zlib.git
 cd zlib
 curl -OL https://raw.githubusercontent.com/shinchiro/mpv-winbuild-cmake/master/packages/zlib-1-win32-static.patch
 patch -p1 -i zlib-1-win32-static.patch
+export PATH="$M_CROSS/bin:$RUSTUP_LOCATION/.cargo/bin:$PATH"
+export PKG_CONFIG="pkgconf --static"
+export PKG_CONFIG_LIBDIR="$M_CROSS/mingw/lib/pkgconfig"
+export RUSTUP_HOME="$RUSTUP_LOCATION/.rustup"
+export CARGO_HOME="$RUSTUP_LOCATION/.cargo"
 CHOST=$MINGW_TRIPLE ./configure --prefix=$M_CROSS/mingw --static
 make -j$MJOBS
 make install
