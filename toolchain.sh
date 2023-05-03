313fae10c3c3436c71d3f54da35b4a9318fbe55b
eko5624 <562433403@qq.com>
2023-05-03 14:14:32 +0800

    Update toolchain.sh

--- toolchain.sh
+++ toolchain.sh
@@ -6,7 +6,6 @@ TOP_DIR=$(pwd)
 # Env Var NUMJOBS overrides automatic detection
 MJOBS=$(grep -c processor /proc/cpuinfo)
 
-MACHINE_TYPE=x86_64
 MINGW_TRIPLE="x86_64-w64-mingw32"
 
 export MINGW_TRIPLE
@@ -44,7 +43,6 @@ git clone https://github.com/mingw-w64/mingw-w64.git --branch master --depth 1
 # <2> build
 echo "building gendef"
 echo "======================="
-
 cd mingw-w64/mingw-w64-tools/gendef
 ./configure --prefix=$M_CROSS
 make -j$MJOBS || echo "(-) Build Error!"
@@ -53,7 +51,6 @@ cd $M_SOURCE
 
 echo "building binutils"
 echo "======================="
-
 cd binutils-2.40
 ./configure \
   --target=$MINGW_TRIPLE \
@@ -70,14 +67,17 @@ cd binutils-2.40
 make -j$MJOBS || echo "(-) Build Error!"
 make install-strip
 
+mkdir -p $M_CROSS/$MINGW_TRIPLE
 cd $M_CROSS
-ln -s $(which pkg-config) bin/$MINGW_TRIPLE-pkg-config
-ln -s $(which pkg-config) bin/$MINGW_TRIPLE-pkgconf
+ln -s $MINGW_TRIPLE mingw
+
+cd $M_CROSS/bin
+ln -s $(which pkg-config) $MINGW_TRIPLE-pkg-config
+ln -s $(which pkg-config) $MINGW_TRIPLE-pkgconf
 cd $M_SOURCE
 
 echo "building mingw-w64-headers"
 echo "======================="
-
 cd mingw-w64/mingw-w64-headers 
 ./configure \
   --host=$MINGW_TRIPLE \
@@ -87,14 +87,10 @@ cd mingw-w64/mingw-w64-headers
   --with-default-msvcrt=msvcrt
 make -j$MJOBS || echo "(-) Build Error!"
 make install install-strip
-
 cd $M_CROSS
-ln -s $MINGW_TRIPLE mingw
-cd $M_SOURCE
 
 echo "building gcc"
 echo "======================="
-
 cd gcc-12-20230421
 # https://gcc.gnu.org/bugzilla/show_bug.cgi?id=54412
 curl -OL https://salsa.debian.org/mingw-w64-team/gcc-mingw-w64/-/raw/5e7d749d80e47d08e34a17971479d06cd423611e/debian/patches/vmov-alignment.patch
@@ -121,7 +117,6 @@ cd $M_SOURCE
 
 echo "building mingw-w64-crt"
 echo "======================="
-
 cd mingw-w64/mingw-w64-crt
 autoreconf -ivf
 ./configure \
@@ -137,7 +132,6 @@ cd $M_SOURCE
 
 echo "building winpthreads"
 echo "======================="
-
 cd mingw-w64/mingw-w64-libraries/winpthreads
 ./configure \
   --host=$MINGW_TRIPLE \
@@ -153,7 +147,6 @@ echo "======================="
 curl -sSf https://sh.rustup.rs | sh -s -- -y --default-toolchain stable --target x86_64-pc-windows-gnu --no-modify-path --profile minimal
 rustup update
 cargo install cargo-c --profile=release-strip --features=vendored-openssl
-
 cat <<EOF >$CARGO_HOME/config
 [net]
 git-fetch-with-cli = true
