9acd6244edd19c40c352c9a7a63ae7c733572714
eko5624 <562433403@qq.com>
2023-05-06 19:35:31 +0800

    Update gcc-with-mcf.sh

--- gcc-with-mcf.sh
+++ gcc-with-mcf.sh
@@ -40,12 +40,37 @@ mkdir -p $M_BUILD
 echo "gettiong source"
 echo "======================="
 cd $M_SOURCE
+
+#binutils
 wget -c -O binutils-2.40.tar.bz2 http://ftp.gnu.org/gnu/binutils/binutils-2.40.tar.bz2
 tar xjf binutils-2.40.tar.bz2
+
+#gcc
 wget -c -O gcc-13.1.0.tar.xz https://ftp.gnu.org/gnu/gcc/gcc-13.1.0/gcc-13.1.0.tar.xz
 xz -c -d gcc-13.1.0.tar.xz | tar xf -
+
+#gmp
+wget -c -O gmp-6.2.1.tar.bz2 https://ftp.gnu.org/gnu/gmp/gmp-6.2.1.tar.bz2
+tar xjf gmp-6.2.1.tar.bz2
+
+#mpfr
+wget -c -O mpfr-4.2.0.tar.bz2 https://ftp.gnu.org/gnu/mpfr/mpfr-4.2.0.tar.bz2
+tar xjf mpfr-4.2.0.tar.bz2
+
+#MPC
+wget -c -O mpc-1.3.1.tar.gz https://ftp.gnu.org/gnu/mpc/mpc-1.3.1.tar.gz
+tar xzf mpc-1.3.1.tar.gz
+
+#isl
+wget -c -O isl-0.24.tar.bz2 https://gcc.gnu.org/pub/gcc/infrastructure/isl-0.24.tar.bz2
+tar xjf isl-0.24.tar.bz2
+
+#mingw-w64
 git clone https://github.com/mingw-w64/mingw-w64.git --branch master --depth 1
+
+#mcfgthread
 git clone https://github.com/lhmouse/mcfgthread.git --branch master --depth 1
+
 
 echo "building gendef"
 echo "======================="
@@ -129,18 +154,80 @@ make -j$MJOBS
 make install
 cd $M_BUILD
 
+echo "building gmp"
+echo "======================="
+mkdir gmp-build
+cd gmp-build
+$M_SOURCE/gmp-6.2.1/configure \
+  --host=$MINGW_TRIPLE \
+  --target=$MINGW_TRIPLE \
+  --prefix=$M_BUILD/for_target \
+  --enable-static \
+  --disable-shared
+make -j$MJOBS
+make install
+cd $M_BUILD
+
+echo "building mpfr"
+echo "======================="
+mkdir mpfr-build
+cd mpfr-build
+$M_SOURCE/mpfr-4.2.0/configure \
+  --host=$MINGW_TRIPLE \
+  --target=$MINGW_TRIPLE \
+  --prefix=$M_BUILD/for_target \
+  --with-gmp=$M_BUILD/for_target \
+  --enable-static \
+  --disable-shared
+make -j$MJOBS
+make install
+cd $M_BUILD
+
+echo "building MPC"
+echo "======================="
+mkdir mpc-build
+cd mpc-build
+$M_SOURCE/mpc-1.3.1/configure \
+  --host=$MINGW_TRIPLE \
+  --target=$MINGW_TRIPLE \
+  --prefix=$M_BUILD/for_target \
+  --with-gmp=$M_BUILD/for_target \
+  --enable-static \
+  --disable-shared
+make -j$MJOBS
+make install
+cd $M_BUILD
+
+echo "building isl"
+echo "======================="
+mkdir isl-build
+cd isl-build
+$M_SOURCE/isl-0.24/configure \
+  --host=$MINGW_TRIPLE \
+  --target=$MINGW_TRIPLE \
+  --prefix=$M_BUILD/for_target \
+  --with-gmp=$M_BUILD/for_target \
+  --enable-static \
+  --disable-shared
+make -j$MJOBS
+make install
+cd $M_BUILD
+
 echo "building gcc"
 echo "======================="
 mkdir gcc-build
 cd gcc-build
 $M_SOURCE/gcc-13.1.0/configure \
-  --build=x86_64-pc-linux-gnu \
   --host=$MINGW_TRIPLE \
   --target=$MINGW_TRIPLE \
   --prefix=$M_TARGET \
   --with-sysroot=$M_TARGET \
   --disable-multilib \
-  --enable-languages=c,c++ \
+  --enable-languages=c,c++,objc,obj-c++ \
+  --with-gmp=$M_BUILD/for_target \
+  --with-mpfr=$M_BUILD/for_target \
+  --with-mpc=$M_BUILD/for_target \
+  --with-isl=$M_BUILD/for_target \
   --disable-nls \
   --disable-werror \
   --disable-shared \
