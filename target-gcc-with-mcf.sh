c51f7ee6da050cefcc62333cec8e46d56b5658d6
eko5624 <562433403@qq.com>
2023-05-09 15:14:37 +0800

    Update target-gcc-with-mcf.sh

--- target-gcc-with-mcf.sh
+++ target-gcc-with-mcf.sh
@@ -137,9 +137,9 @@ patch -d $M_SOURCE/mingw-w64 -p1 < 9001-crt-Mark-atexit-as-DATA-because-it-s-alw
 patch -d $M_SOURCE/mingw-w64 -p1 < 9002-crt-Provide-wrappers-for-exit-in-libmingwex.patch
 patch -d $M_SOURCE/mingw-w64 -p1 < 9003-crt-Implement-standard-conforming-termination-suppor.patch
 patch -d $M_SOURCE/mingw-w64 -p1 < 9004-crt-Copy-clock-and-nanosleep-from-winpthreads.patch
+patch -d $M_SOURCE/mingw-w64 -p1 < 0001-Allow-to-use-bessel-and-complex-functions-without-un.patch
 cd $M_SOURCE/mingw-w64/mingw-w64-crt
 autoreconf -ivf
-patch -d $M_SOURCE/mingw-w64 -p1 < $M_BUILD/crt-build/0001-Allow-to-use-bessel-and-complex-functions-without-un.patch
 cd $M_BUILD/crt-build
 $M_SOURCE/mingw-w64/mingw-w64-crt/configure \
   --host=$MINGW_TRIPLE \
