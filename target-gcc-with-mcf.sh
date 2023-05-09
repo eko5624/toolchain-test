ed7f3bca72b99d0f7201f98f7e0e88dcaaa8653d
eko5624 <562433403@qq.com>
2023-05-09 16:10:40 +0800

    Update target-gcc-with-mcf.sh

--- target-gcc-with-mcf.sh
+++ target-gcc-with-mcf.sh
@@ -270,7 +270,12 @@ $M_SOURCE/gcc-13.1.0/configure \
   --host=$MINGW_TRIPLE \
   --target=$MINGW_TRIPLE \
   --prefix=$M_TARGET \
+  --with-local-prefix=$M_TARGET/$MINGW_TRIPLE \
+  --with-native-system-header-dir=$M_TARGET/$MINGW_TRIPLE/include \
+  --libexecdir=$M_TARGET/$MINGW_TRIPLE/lib \
+  --enable-bootstrap \
   --with-sysroot=$M_TARGET \
+  --disable-libssp \
   --disable-multilib \
   --enable-languages=c,c++ \
   --with-gmp=$M_BUILD/for_target \
