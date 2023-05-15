c77bb20a5e52d40c122fecf6091aecbd013b1abe
eko5624 <562433403@qq.com>
2023-05-15 22:37:12 +0800

    Update target-gcc-with-mcf.sh

--- target-gcc-with-mcf.sh
+++ target-gcc-with-mcf.sh
@@ -290,7 +290,7 @@ sed -i 's/${prefix}\/mingw\//${prefix}\//g' configure
 # change hardcoded /mingw prefix to the real prefix .. isn't this rubbish?
 # it might work at build time and could be important there but beyond that?!
 export MINGW_NATIVE_PREFIX=$M_TARGET
-sed -i "s#\\/mingw\\/#${MINGW_NATIVE_PREFIX//\//\\/}\\/#g" gcc/config/i386/mingw32.h
+sed -i "s#/mingw/#${MINGW_NATIVE_PREFIX}/#g" gcc/config/i386/mingw32.h
 
 # so libgomp DLL gets built despide static libdl
 export lt_cv_deplibs_check_method='pass_all'
