7ce6c50029c0c3b38998712de15e70ae14aa57e3
eko5624 <562433403@qq.com>
2023-06-03 13:26:43 +0800

    Update cross-gcc-ucrt.sh

--- cross-gcc-ucrt.sh
+++ cross-gcc-ucrt.sh
@@ -7,10 +7,10 @@ TOP_DIR=$(pwd)
 # Env Var NUMJOBS overrides automatic detection
 MJOBS=$(grep -c processor /proc/cpuinfo)
 
-CFLAGS="-pipe -O2"
+#CFLAGS="-pipe -O2"
 MINGW_TRIPLE="x86_64-w64-mingw32"
 export MINGW_TRIPLE
-export CFLAGS
+#export CFLAGS
 export CXXFLAGS=$CFLAGS
 
 export M_ROOT=$(pwd)
