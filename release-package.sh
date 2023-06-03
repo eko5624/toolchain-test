217586a1b07a81325ca476796959eac7a952697d
eko5624 <562433403@qq.com>
2023-06-03 16:00:58 +0800

    Update release-package.sh

--- release-package.sh
+++ release-package.sh
@@ -1,27 +1,35 @@
 #!/bin/bash
-set -x  
-CURL=/d/ucrt64/bin/curl
+set -ex
 CURL_RETRIES="--connect-timeout 60 --retry 5 --retry-delay 5 --http1.1"
 
-#Get mpv latest commit sha
-short_sha=$(cat /d/msys64/opt/mpv/SHORT_SHA)
+# Delete assets
+asset_id=($(curl -u $GITHUB_ACTOR:$GH_TOKEN $CURL_RETRIES \
+  -H "Accept: application/vnd.github.v3+json" \
+  https://api.github.com/repos/${GITHUB_REPOSITORY}/releases/tags/toolchain \
+| jq -r '.assets[] | select(.name | startswith("'"$1"'")) | .id'))
+
+for id in "${asset_id[@]}"; do
+  curl -u $GITHUB_ACTOR:$GH_TOKEN $CURL_RETRIES \
+    -X DELETE \
+    -H "Accept: application/vnd.github.v3+json" \
+    https://api.github.com/repos/${GITHUB_REPOSITORY}/releases/assets/$id;
+done    
 
 # Release assets
-date=$(date +%Y-%m-%d)
-
-$CURL -u $GITHUB_ACTOR:$GH_TOKEN $CURL_RETRIES \
+curl -u $GITHUB_ACTOR:$GH_TOKEN $CURL_RETRIES \
   -X POST \
   -H "Accept: application/vnd.github.v3+json" \
   https://api.github.com/repos/${GITHUB_REPOSITORY}/releases \
-  -d '{"tag_name":"'"$date"'","name":"'"$date"'","body":"Bump to mpv-player/mpv@'"$short_sha"'"}'
+  -d '{"tag_name": "toolchain"}'
   
-release_id=$($CURL -u $GITHUB_ACTOR:$GH_TOKEN $CURL_RETRIES \
+release_id=$(curl -u $GITHUB_ACTOR:$GH_TOKEN $CURL_RETRIES \
   -H "Accept: application/vnd.github.v3+json" \
-  https://api.github.com/repos/${GITHUB_REPOSITORY}/releases/tags/$date | jq -r '.id')
+  https://api.github.com/repos/${GITHUB_REPOSITORY}/releases/tags/toolchain | jq -r '.id')
   
-for f in *.7z; do
-  $CURL -u $GITHUB_ACTOR:$GH_TOKEN $CURL_RETRIES \
+for f in *.zip; do 
+  curl -u $GITHUB_ACTOR:$GH_TOKEN $CURL_RETRIES \
     -X POST -H "Accept: application/vnd.github.v3+json" \
     -H "Content-Type: $(file -b --mime-type $f)" \
-    https://uploads.github.com/repos/${GITHUB_REPOSITORY}/releases/$release_id/assets?name=$(basename $f) --data-binary @$f; 
+    https://uploads.github.com/repos/${GITHUB_REPOSITORY}/releases/$release_id/assets?name=$(basename $f) \
+    --data-binary @$f; 
 done
