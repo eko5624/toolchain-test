bbf8ef2cf4e4f3ed809ee0e36273c37e22e9464b
eko5624 <562433403@qq.com>
2023-06-06 00:24:38 +0800

    Update target-gcc-with-posix.sh

--- target-gcc-with-posix.sh
+++ target-gcc-with-posix.sh
@@ -487,6 +487,7 @@ make -j$MJOBS
 make install
 #mv $M_TARGET/lib/libgcc_s_seh-1.dll $M_TARGET/bin/
 cp $M_TARGET/bin/gcc.exe $M_TARGET/bin/cc.exe
+cp $M_TARGET/bin/$MINGW_TRIPLE-gcc.exe $M_TARGET/bin/$MINGW_TRIPLE-cc.exe
 
 echo "building make"
 echo "======================="
