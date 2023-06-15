bd3f559c7a388f3e6c9aca6787fb195b11c0b8cf
eko5624 <562433403@qq.com>
2023-06-15 22:35:34 +0800

    Update target-gcc-with-mcf.sh

--- target-gcc-with-mcf.sh
+++ target-gcc-with-mcf.sh
@@ -719,8 +719,6 @@ patch -ulbf configure << EOF
 EOF
 ## fix linker error: export ordinal too large (version >= 13)
 sed -i.bak "s/--export-all-symbols/--gc-keep-exported/" $(grep -l "\--export-all-symbols" $(find . -name configure))
-# fix detection of GMP/MPFR/MPC
-sed -i.bak -e  "s/#include [<\"]\(gmp\|mpc\|mpfr\|isl\)\.h[>\"]/#include <stdio.h>\n&/" configure
 # fix missing mmap/munmap and linker error: export ordinal too large
 sed -i.bak "s/\(\${wl}\)--export-all-symbols/\1--gc-keep-exported \1-lmman/" $(grep -l "\${wl}--export-all-symbols" $(find . -name configure))
 # fix detection of GMP/MPFR/MPC
