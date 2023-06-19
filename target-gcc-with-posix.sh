0f6bfe102d5fc4cd8299d2ed146933a0412a5b63
eko5624 <562433403@qq.com>
2023-06-19 19:57:22 +0800

    Update target-gcc-with-posix.sh

--- target-gcc-with-posix.sh
+++ target-gcc-with-posix.sh
@@ -226,6 +226,7 @@ $M_SOURCE/gcc-13.1.0/configure \
   --host=$MINGW_TRIPLE \
   --target=$MINGW_TRIPLE \
   --prefix=$M_TARGET \
+  --libexecdir=$M_TARGET/lib \
   --with-sysroot=$M_TARGET \
   --with-{gmp,mpfr,mpc,isl}=$M_BUILD/for_target \
   --disable-multilib \
